700ded81c7bdec4636c8121fb6a2376840aa0bbc
nazariiko <nazarii.dev@gmail.com>
2021-09-11 15:48:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Header, Content } from './components';
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Content />
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/ContentProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SneakersCard } from '../components'
+
+function ContentProducts() {
+  return (
+    <div className="content-products">
+      <SneakersCard />
+    </div>
+  )
+}
+
+export default ContentProducts
